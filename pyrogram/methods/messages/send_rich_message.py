f5d7985ae4f334ec73898d069177711c1f56b2c1
Abdullah <aldheeb01@gmail.com>
2026-09-19 22:29:55 +0300

    fix: include `InputPeerUserFromMessage` in user type checks across chat methods

--- pyrogram/methods/messages/send_rich_message.py
+++ pyrogram/methods/messages/send_rich_message.py
@@ -165,7 +165,11 @@ class SendRichMessage:
         if isinstance(r, raw.types.UpdateShortSentMessage):
             peer = await self.resolve_peer(chat_id)
 
-            peer_id = peer.user_id if isinstance(peer, raw.types.InputPeerUser) else -peer.chat_id
+            peer_id = (
+                peer.user_id 
+                if isinstance(peer, (raw.types.InputPeerUser, raw.types.InputPeerUserFromMessage)) 
+                else -peer.chat_id
+            )
 
             return types.Message(
                 id=r.id,
